0c435839b94611b8161fd242de69096075e3952f
Leonard Martin <116867789+lmartin2422@users.noreply.github.com>
2024-02-02 18:34:40 -0500

    Update Tag.ts yello color

--- src/app/_models/Tag.ts
+++ src/app/_models/Tag.ts
@@ -1,18 +1,21 @@
 // the color coordinated languages on the portfolio cards
 
 export class Tag {
-    static readonly ANGULAR = new Tag('Angular', 'blue'); // defined a tag called Angular, static readonly acts as enums
-    static readonly TYPESCRIPT = new Tag('Typescript', 'blue'); 
-    static readonly PYTHON = new Tag('Python', 'royal blue'); 
-    static readonly JAVA = new Tag('Java', 'blue'); 
-    static readonly NODEJS = new Tag('Node.JS', 'blue'); 
-    static readonly JAVASCRIPT = new Tag('Javascript', 'blue'); 
-    static readonly UIPATH = new Tag('UiPath', 'blue'); 
-    static readonly HTML = new Tag('HTML/CSS', 'blue');
-    static readonly VBNET = new Tag('Vb.Net', 'blue');
-    static readonly FLASK = new Tag('Flask', 'blue');
-    static readonly EXPRESSJS = new Tag('Express.JS', 'blue');
-    static readonly SPRINGBOOT = new Tag('SpringBoot', 'blue')
+    static readonly ANGULAR = new Tag('Angular', 'yellow'); // defined a tag called Angular, static readonly acts as enums
+    static readonly TYPESCRIPT = new Tag('Typescript', 'yellow'); 
+    static readonly PYTHON = new Tag('Python', 'yellow'); 
+    static readonly JAVA = new Tag('Java', 'yellow'); 
+    static readonly NODEJS = new Tag('Node.JS', 'yellow'); 
+    static readonly JAVASCRIPT = new Tag('Javascript', 'yellow'); 
+    static readonly UIPATH = new Tag('UiPath', 'yellow'); 
+    static readonly HTML = new Tag('HTML/CSS', 'yellow');
+    static readonly VBNET = new Tag('Vb.Net', 'yellow');
+    static readonly FLASK = new Tag('Flask', 'yellow');
+    static readonly EXPRESSJS = new Tag('Express.JS', 'yellow');
+    static readonly REACT = new Tag('React', 'yellow');
+    static readonly SPRINGBOOT = new Tag('SpringBoot', 'yellow')
+
+
 
 
     private constructor(private readonly key: string, public readonly color: string) {
